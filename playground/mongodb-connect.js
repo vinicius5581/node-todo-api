32ac38baeef06716937e00f2c66ca6119156954b
Vinicius Santana <santanaviny@gmail.com>
2017-10-10 19:36:27 -0700

    ._id.getTimestamp()

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,4 +1,5 @@
-const MongoClient = require('mongodb').MongoClient;
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
 
 MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
   if (err) {
@@ -16,17 +17,18 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
   //   console.log(JSON.stringify(result.ops, undefined, 2));
   // });
   //
-  // // Insert new doc into Users (name, age, location)
-  // db.collection('Users').insertOne({
-  //   name: 'Vini',
-  //   age: 36,
-  //   location: 'California'
-  // }, (err, result) => {
-  //   if (err) {
-  //     return console.log('Unable to insert user', err);
-  //   }
-  //   console.log(result.ops);
-  // });
+  // Insert new doc into Users (name, age, location)
+  db.collection('Users').insertOne({
+    name: 'Vini',
+    age: 36,
+    location: 'California'
+  }, (err, result) => {
+    if (err) {
+      return console.log('Unable to insert user', err);
+    }
+    console.log(result.ops);
+    console.log(result.ops[0]._id.getTimestamp());
+  });
 
   db.close();
 });
