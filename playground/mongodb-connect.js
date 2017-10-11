0dc6f4db4b3472c77f75f1429b769507aa5e87ad
Vinicius Santana <santanaviny@gmail.com>
2017-10-10 18:01:39 -0700

    Testing connect

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -6,5 +6,27 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
   }
   console.log('Connected to MongoDB server');
 
+  // db.collection('Todos').insertOne({
+  //   text: 'Something to do',
+  //   completed: false
+  // }, (err, result) => {
+  //   if (err) {
+  //     return console.log('Unable to insert todo', err);
+  //   }
+  //   console.log(JSON.stringify(result.ops, undefined, 2));
+  // });
+  //
+  // // Insert new doc into Users (name, age, location)
+  // db.collection('Users').insertOne({
+  //   name: 'Vini',
+  //   age: 36,
+  //   location: 'California'
+  // }, (err, result) => {
+  //   if (err) {
+  //     return console.log('Unable to insert user', err);
+  //   }
+  //   console.log(result.ops);
+  // });
+
   db.close();
 });
