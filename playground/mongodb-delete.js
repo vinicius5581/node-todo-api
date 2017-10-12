28c15d3f71b32f34810ad15f2156eb8dcd53e3de
Vinicius Santana <santanaviny@gmail.com>
2017-10-12 01:43:47 -0700

    Delete todos by id

--- playground/mongodb-delete.js
+++ playground/mongodb-delete.js
@@ -8,24 +8,24 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
   console.log('Connected to MongoDB server');
 
   // deleteMany
-  db.collection('Todos').deleteMany({value: 'baum de mais'}).then((result) => {
-    console.log(result);
-  });
+  // db.collection('Todos').deleteMany({text: 'Eat lunch'}).then((result) => {
+  //   console.log(result);
+  // });
 
   // deleteOne
-  db.collection('Todos').deleteOne({text: 'Eat lunch'}).then((result) => {
-    console.log(result);
-  });
+  // db.collection('Todos').deleteOne({text: 'Eat lunch'}).then((result) => {
+  //   console.log(result);
+  // });
 
   // findOneAndDelete
-  db.collection('Todos').findOneAndDelete({completed: false}).then((result) => {
-    console.log(result);
-  });
+  // db.collection('Todos').findOneAndDelete({completed: false}).then((result) => {
+  //   console.log(result);
+  // });
 
-  db.collection('Users').deleteMany({name: 'Vini'});
+  // db.collection('Users').deleteMany({name: 'Andrew'});
 
   db.collection('Users').findOneAndDelete({
-    _id: new ObjectID("59dd2ce324ae8f95de21dbfc")
+    _id: new ObjectID("57ac8d47878a299e5dc21bc8")
   }).then((results) => {
     console.log(JSON.stringify(results, undefined, 2));
   });
